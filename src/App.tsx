d846b237f4ca7cff5c31447b469187e70804946a
Pikadorius <karpukea@yandex.by>
2022-12-05 15:55:08 +0300

    deleted all logic from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import Counter from "./Counter/Counter";
-import {useDispatch, useSelector} from 'react-redux';
-
-export type ErrorType = '' | 'Incorrect value!'
-export type StateType = {
-    minValue: number
-    maxValue: number
-    currentValue: number
-    error: ErrorType
-    isEditMode: boolean
-}
 
 function App() {
-    const [state, setState] = useState<StateType>(    {minValue: 0, currentValue: 0, maxValue: 5, error: '', isEditMode: false}
-    )
-
-    const stateCounter=useSelector<StateType>(state=>state)
-    const dispatch=useDispatch()
-
-    useEffect(()=>{
-       let localState=localStorage.getItem('state')
-        localState && setState(JSON.parse(localState))
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem('state', JSON.stringify(state))
-    }, [state])
-
-
-    const increase = () => {
-        const checkStartValue =  state.currentValue < state.maxValue
-        checkStartValue && setState({...state, currentValue: state.currentValue + 1, error: ''})
-    }
-
-    const reset = () => {
-        setState({...state, currentValue: state.minValue, error: ''})
-    }
-
-    const setEditMode = (isEditMode: boolean) => {
-        setState({...state, isEditMode: !isEditMode})
-    }
-
-    const setMinValue = (minValue: number) => {
-        if (minValue >= 0 && minValue < state.maxValue) {
-            setState({...state, minValue: minValue, currentValue: minValue, error: ''})
-        } else {
-            setState({...state, minValue, currentValue: state.minValue, error: "Incorrect value!"})
-        }
-    }
-
-    const setMaxValue = (maxValue: number) => {
-        if (maxValue > state.minValue && state.minValue >= 0) {
-            setState({...state, maxValue, currentValue: state.minValue, error: ''})
-        } else setState({...state, maxValue, currentValue: state.minValue, error: "Incorrect value!"})
-    }
 
 
     return (
         <div className="App">
-            <Counter
-                state={state}
-                increase={increase}
-                reset={reset}
-                setMinValue={setMinValue}
-                setMaxValue={setMaxValue}
-                setEditMode={setEditMode}/>
+            <Counter/>
         </div>
     );
 }
